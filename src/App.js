4f764906e3bdc9b1e65eaa440546b56db12c4faf
alphonso06 <43776043+alphonso06@users.noreply.github.com>
2020-12-29 19:06:36 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { grommet, Grommet } from "grommet";
+
+export default function App() {
+  return (
+    <Grommet theme={grommet} full>
+      hello world
+    </Grommet>
+  );
+}
